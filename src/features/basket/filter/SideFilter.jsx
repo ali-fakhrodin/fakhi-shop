b762637f6c3d582abda0a977acbcfcae17f4e070
AliFakhrodin <alifakhrodin16@gmail.com>
2025-06-22 12:44:42 +0330

    create: SideFilter.jsx

---
+++ src/features/basket/filter/SideFilter.jsx
@@ -0,0 +1,5 @@
+function SideFilter() {
+  return <div>Filter</div>;
+}
+
+export default SideFilter;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,7 +2,7 @@ function Button({ text, type }) {
   return (
     <button
       type={type}
-      className="border-none rounded-lg px-4 py-[2px] bg-slate-200 text-slate-600 text-[15px] font-[yekan_bakh] font-semibold "
+      className="border-none rounded-lg px-4 py-[2px] bg-slate-200 text-slate-600 text-[15px] font-semibold "
     >
       + {text}
     </button>
