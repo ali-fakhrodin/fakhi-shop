62e01c91447ba0599dad955e3804c20176bc4008
AliFakhrodin <alifakhrodin16@gmail.com>
2025-06-28 00:43:41 +0330

    add swiper

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,40 @@
-import styled from "styled-components";
-import SideFilter from "../features/basket/filter/SideFilter";
-import SwiperBox from "../ui/SwiperBox";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-const StyledContainer = styled.div`
-  grid-template-columns: 2fr 10fr;
-`;
+import SwiperBox from "../ui/SwiperBox";
 
 function Home() {
   return (
-    <StyledContainer className="grid">
-      <div>
-        <SideFilter />
+    <>
+      <div className="h-[80vh] flex items-center justify-center">
+        {" "}
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          autoplay={{ delay: 3000 }}
+          keyboard={{ enabled: true }}
+          navigation={{ clickable: true }}
+          pagination={{ clickable: true }}
+          spaceBetween={50}
+          slidesPerView={1}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+        </Swiper>
       </div>
       <div>
         <SwiperBox title="بیشترین تخفیف" path="/products"></SwiperBox>
         <SwiperBox title="کالای مورد علاقه" path="/products"></SwiperBox>
         <SwiperBox title="خریدی به صرفه" path="/products"></SwiperBox>
       </div>
-    </StyledContainer>
+    </>
   );
 }
 
