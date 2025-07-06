f77702224d3a31a9dda38073890ae00660714f75
AliFakhrodin <alifakhrodin16@gmail.com>
2025-07-06 21:59:59 +0330

    login logic

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,23 +1,59 @@
+import { useState } from "react";
 import Button from "../../ui/Button";
 import Input from "../../ui/Input";
+import { useLogin } from "./useLogin";
 
 function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, isLoading } = useLogin();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+    login({ email, password });
+  }
+
   return (
     <div className="flex flex-col gap-2 items-center justify-center h-[100%]">
       <div className="w-full flex flex-col items-center justify-center gap-2">
-        <label className="font-[yekan-b]" for="email">
+        <label className="font-[yekan-b]" htmlFor="email">
           ایمیل
         </label>
-        <Input type="email" placeholder="example@gmail.com" id="email" dir="ltr"/>
+        <Input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          type="email"
+          placeholder="example@gmail.com"
+          id="email"
+          dir="ltr"
+          autoComplete="email"
+          disabled={isLoading}
+        />
       </div>
 
       <div className="w-full flex flex-col items-center justify-center gap-2 mb-3">
-        <label className="font-[yekan-b]" for="password">
+        <label className="font-[yekan-b]" htmlFor="password">
           پسوورد
         </label>
-        <Input type="password" placeholder="Password" id="password" dir="ltr"/>
+        <Input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          placeholder="Password"
+          id="password"
+          dir="ltr"
+          autoComplete="Password"
+          disabled={isLoading}
+        />
       </div>
-      <Button type="submit" style="primary" text="تایید" />
+      <Button
+        type="submit"
+        style="primary"
+        text={isLoading ? "صبر کنید!" : "تایید"}
+        disabled={isLoading}
+        onClick={handleSubmit}
+      />
     </div>
   );
 }
