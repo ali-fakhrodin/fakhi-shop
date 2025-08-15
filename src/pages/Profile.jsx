59a5d21ca54f80f477be76706463a959dad42666
AliFakhrodin <alifakhrodin16@gmail.com>
2025-08-15 13:39:06 +0330

    use of OrderBox and get all data from database

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import { useUser } from "../features/authentication/useUser";
 import { useOrders } from "../features/orders/useOrders";
+import OrderBox from "../ui/orderBox";
 
 function Profile() {
   const {
@@ -8,25 +9,11 @@ function Profile() {
 
   const { isLoading, orders } = useOrders();
 
-  if (!isLoading) console.log(orders, isLoading);
-
-  // <p>لیست سفارشات شما</p>
   return (
-    <div className="flex flex-col md:flex-row justify-around items-center bg-slate-400 ">
-      <div className="w-full md:w-1/2 flex items-center md:items-start flex-col gap-1 h-[20rem] overflow-y-scroll my-4 px-2">
+    <div className="flex flex-col md:flex-row justify-around items-center bg-slate-400 h-[54vh] mt-4 rounded-xl overflow-y-scroll">
+      <div className="w-full md:w-1/2 flex items-center md:items-start flex-col gap-1 h-[23rem] overflow-y-scroll px-4">
         {!isLoading
-          ? orders.data.map((order) => (
-              <div className="w-[75%] md:w-full bg-slate-600 rounded-lg overflow-hidden flex gap-x-4 py-2 px-4 min-h-[3.3rem] sm:min-h-[4rem] text-sm sm:text-base">
-                <div>
-                  <p className="text-white">شماره سفارش: {order.id}</p>
-                  <p className="text-white">تعداد محصول</p>
-                </div>
-                <div>
-                  <p className="text-white">مجموع مبلغ: 0</p>
-                  <p className="text-white">وضعیت: تحویل داده شده</p>
-                </div>
-              </div>
-            ))
+          ? orders.data.map((order) => <OrderBox data={order} />)
           : null}
       </div>
 
@@ -35,10 +22,10 @@ function Profile() {
           <input
             type="email"
             value={user.email}
-            className=" w-[70%] py-2 px-3 rounded-md"
+            className="w-[70%] py-2 px-3 rounded-md"
             dir="ltr"
           />
-          ایمیل
+          <span className="text-white">ایمیل</span>
         </div>
         <div className="flex items-center justify-center gap-2 w-full text-sm md:text-[16px]">
           <input
@@ -46,7 +33,7 @@ function Profile() {
             className="w-[65%] py-2 px-3 rounded-md"
             dir="ltr"
           />
-          شماره همراه
+          <span className="text-white">شماره همراه</span>
         </div>
       </form>
     </div>
--- src/ui/orderBox.jsx
+++ src/ui/orderBox.jsx
@@ -1,5 +1,20 @@
-function orderBox({ order }) {
-  return <div></div>;
+import { format, formatDate } from "date-fns";
+
+function OrderBox({ data }) {
+  const formatedDate = format(new Date(data.created_at), "yyyy/MM/dd");
+  return (
+    <div className="w-[75%] md:w-full bg-slate-600 rounded-lg overflow-hidden flex gap-x-4 py-2 px-4 min-h-[3.3rem] sm:min-h-[4rem] text-sm sm:text-base">
+      <div>
+        <p className="text-white">شماره سفارش: {data.id}</p>
+        <p className="text-white">تعداد محصول: {data.numberOfProducts}</p>
+      </div>
+      <div>
+        <p className="text-white">مجموع مبلغ: {data.orderCost}</p>
+        <p className="text-white">وضعیت: {data.orderStatus}</p>
+      </div>
+      <p className="text-zinc-200 self-center flex-grow text-left">{formatedDate}</p>
+    </div>
+  );
 }
 
-export default orderBox;
+export default OrderBox;
