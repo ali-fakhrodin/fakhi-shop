82c743d3e8c181a44f91d0dbe1e5c63c6e587277
AliFakhrodin <alifakhrodin16@gmail.com>
2025-08-15 16:19:29 +0330

    edit responsive

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,14 +10,14 @@ function Profile() {
   const { isLoading, orders } = useOrders();
 
   return (
-    <div className="flex flex-col md:flex-row justify-around items-center bg-slate-400 h-[54vh] mt-4 rounded-xl overflow-y-scroll">
-      <div className="w-full md:w-1/2 flex items-center md:items-start flex-col gap-1 h-[23rem] overflow-y-scroll px-4">
+    <div className="flex flex-col md:flex-row justify-around items-center bg-slate-400 min-h-[54vh] mt-4 rounded-xl overflow-y-scroll">
+      <div className="w-full md:w-1/2 flex items-center md:items-start flex-col gap-1 h-[23rem] overflow-y-scroll py-4 px-4">
         {!isLoading
           ? orders.data.map((order) => <OrderBox data={order} />)
           : null}
       </div>
 
-      <form className="h-[50vh] flex w-1/2 flex-col justify-center items-center gap-3">
+      <form className="h-[25vh] flex w-1/2 flex-col justify-center items-center gap-3">
         <div className="flex items-center justify-center gap-2 w-full text-sm md:text-[16px]">
           <input
             type="email"
--- src/ui/orderBox.jsx
+++ src/ui/orderBox.jsx
@@ -3,7 +3,7 @@ import { format, formatDate } from "date-fns";
 function OrderBox({ data }) {
   const formatedDate = format(new Date(data.created_at), "yyyy/MM/dd");
   return (
-    <div className="w-[75%] md:w-full bg-slate-600 rounded-lg overflow-hidden flex gap-x-4 py-2 px-4 min-h-[3.3rem] sm:min-h-[4rem] text-sm sm:text-base">
+    <div className="w-full sm:w-[95%] md:w-full bg-slate-600 rounded-lg overflow-hidden flex gap-x-1 items-center sm:gap-x-4 py-2 px-4 min-h-[3.3rem] h-auto sm:min-h-[4rem] text-[12px] sm:text-sm lg:text-base">
       <div>
         <p className="text-white">شماره سفارش: {data.id}</p>
         <p className="text-white">تعداد محصول: {data.numberOfProducts}</p>
