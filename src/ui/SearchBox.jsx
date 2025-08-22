f8c65ce056a9e0f046523a533202c4f0f7c8b662
AliFakhrodin <alifakhrodin16@gmail.com>
2025-08-22 10:55:10 +0330

    start search car

--- src/ui/SearchBox.jsx
+++ src/ui/SearchBox.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import { BiSearch } from "react-icons/bi";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function SearchBox() {
+  const navigate = useNavigate();
+  const [searchParam, setSearchParam] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  function searchInCars() {
+    searchParams.set("carName", "نیسان");
+    setSearchParams(searchParams);
+
+    window.location.pathname !== "/products" && navigate("/products");
+  }
+
   return (
     <div className="relative">
       <input
         type="search"
         placeholder="جست و جوی ماشین ..."
         className="rounded-lg md:w-44 xl:w-72 text-md pr-7 py-1 tracking-wider"
+        onKeyDown={searchInCars}
+        value={searchParam}
+        onChange={(e) => setSearchParam(e.target.value)}
       />
       <button className="absolute z-10 text-slate-600 top-[20%] right-1 size-5">
         <BiSearch className="size-5" />
